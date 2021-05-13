99fec0900c192dc8764be33d19ffb785a49b88c7
Caroline Salib <carolinesalibc@gmail.com>
2021-05-13 08:14:53 -0600

    feat: add components for maintenance list and rows
    Starting to understand how components work
    TODO: need to find a better way to deal with css

---
+++ test/components/maintenance_list_component_test.rb
@@ -0,0 +1,10 @@
+require "test_helper"
+
+class MaintenanceListComponentTest < ViewComponent::TestCase
+  def test_component_renders_something_useful
+    # assert_equal(
+    #   %(<span>Hello, components!</span>),
+    #   render_inline(MaintenanceListComponent.new(message: "Hello, components!")).css("span").to_html
+    # )
+  end
+end
