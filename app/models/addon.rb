af295311aec6cba663ff7a4a1d531019cf0a494f
Caroline Salib <carolinesalibc@gmail.com>
2021-06-16 08:19:08 -0600

    Include broadcasts to addon model

--- app/models/addon.rb
+++ app/models/addon.rb
@@ -1,3 +1,7 @@
 class Addon < ApplicationRecord
   belongs_to :heroku_app
+
+  after_create_commit { broadcast_prepend_to 'addons' }
+  after_update_commit { broadcast_replace_to 'addons' }
+  after_destroy_commit { broadcast_remove_to 'addons' }
 end
